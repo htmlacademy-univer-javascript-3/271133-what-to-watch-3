44a4ccf310d4ae0130d1ed51eacda9c912a23a4c
timpo <timpo@mail.ru>
2023-11-09 00:08:09 +0300

    Больше подробностей (часть 1)

--- src/CommonComponent/MovieCard/MovieCard.tsx
+++ src/CommonComponent/MovieCard/MovieCard.tsx
@@ -1,32 +1,37 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Player } from './Player';
 
 export type MovieCardProps = {
     id: number;
     name: string;
     imgSrc: string;
-    onMouseEnter?: () => void;
-    onMouseLeave?: () => void;
+    videoSrc: string;
 };
 export const MovieCard = ({
   id,
   name,
   imgSrc,
-  onMouseEnter,
-  onMouseLeave,
-}: MovieCardProps) => (
-  <article className="small-film-card catalog__films-card"
-    onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}
-  >
-    <div className="small-film-card__image">
-      <img
-        src={imgSrc}
-        alt={name}
-        width="280"
-        height="175"
-      />
-    </div>
-    <h3 className="small-film-card__title">
-      <Link className="small-film-card__link" to={`/films/${id}`}>{name}</Link>
-    </h3>
-  </article>
-);
+  videoSrc
+}: MovieCardProps) => {
+  const [isHovered, setIsHovered] = useState(false);
+  return (
+    <article
+      className="small-film-card catalog__films-card"
+      onMouseEnter={() => {
+        setIsHovered(true);
+      }}
+      onMouseLeave={() => {
+        setIsHovered(false);
+      }}
+    >
+      <div className="small-film-card__image">
+        <Player videoSrc={videoSrc} imgSrc={imgSrc} isHovered={isHovered} />
+      </div>
+      <h3 className="small-film-card__title">
+        <Link className="small-film-card__link" to={`/films/${id}`}>{name}</Link>
+      </h3>
+    </article>
+  );
+};
+
--- src/CommonComponent/MoviesList/MoviesList.tsx
+++ src/CommonComponent/MoviesList/MoviesList.tsx
@@ -1,26 +1,16 @@
 import { MovieCard, MovieCardProps } from '../MovieCard/MovieCard';
-import { useState } from 'react';
 
 type MoviesListProps = {
     movies: MovieCardProps[];
 };
 
-export const MoviesList = ({movies}: MoviesListProps) => {
-  const [, setActiveFilm] = useState<number | null>();
-  return (
-    <div className="catalog__films-list">
-      {movies.map((movie) => (
-        <MovieCard
-          key={movie.id}
-          {...movie}
-          onMouseEnter={() => {
-            setActiveFilm(movie.id);
-          }}
-          onMouseLeave={() => {
-            setActiveFilm(null);
-          }}
-        />
-      ))}
-    </div>
-  );
-};
+export const MoviesList = ({movies}: MoviesListProps) => (
+  <div className="catalog__films-list">
+    {movies.map((movie) => (
+      <MovieCard
+        key={movie.id}
+        {...movie}
+      />
+    ))}
+  </div>
+);
