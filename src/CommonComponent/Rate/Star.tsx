028d988eab055cd17d904eb63d33d6eb49374e3b
timpo <timpo@mail.ru>
2023-11-08 23:16:13 +0300

    Обратная сторона реальности

---
+++ src/CommonComponent/Rate/Star.tsx
@@ -0,0 +1,7 @@
+import { Star } from './Star.tsx';
+
+const MAX_RATE = 10;
+
+type RateProps = {
+    onClick: (value: number) => void;
+};
---
+++ src/Mocks/Movies.ts
@@ -0,0 +1,102 @@
+export const MOVIES = [
+  {
+    id: 1,
+    name: 'Fantastic Beasts: The Crimes of Grindelwald',
+    imgSrc: 'img/fantastic-beasts-the-crimes-of-grindelwald.jpg',
+  },
+  {
+    id: 2,
+    name: 'Bohemian Rhapsody',
+    imgSrc: 'img/bohemian-rhapsody.jpg',
+  },
+  {
+    id: 3,
+    name: 'Macbeth',
+    imgSrc: 'img/macbeth.jpg',
+  },
+  {
+    id: 4,
+    name: 'Aviator',
+    imgSrc: 'img/aviator.jpg',
+  },
+  {
+    id: 5,
+    name: 'We need to talk about Kevin',
+    imgSrc: 'img/we-need-to-talk-about-kevin.jpg',
+  },
+  {
+    id: 6,
+    name: 'What We Do in the Shadows',
+    imgSrc: 'img/what-we-do-in-the-shadows.jpg',
+  },
+  {
+    id: 7,
+    name: 'Revenant',
+    imgSrc: 'img/revenant.jpg',
+  },
+  {
+    id: 8,
+    name: 'Johnny English',
+    imgSrc: 'img/johnny-english.jpg',
+  },
+  {
+    id: 9,
+    name: 'Shutter Island',
+    imgSrc: 'img/shutter-island.jpg',
+  },
+  {
+    id: 10,
+    name: 'Pulp Fiction',
+    imgSrc: 'img/pulp-fiction.jpg',
+  },
+  {
+    id: 11,
+    name: 'No Country for Old Men',
+    imgSrc: 'img/no-country-for-old-men.jpg',
+  },
+  {
+    id: 12,
+    name: 'Snatch',
+    imgSrc: 'img/snatch.jpg',
+  },
+  {
+    id: 13,
+    name: 'Moonrise Kingdom',
+    imgSrc: 'img/moonrise-kingdom.jpg',
+  },
+  {
+    id: 14,
+    name: 'Seven Years in Tibet',
+    imgSrc: 'img/seven-years-in-tibet.jpg',
+  },
+  {
+    id: 15,
+    name: 'Midnight Special',
+    imgSrc: 'img/midnight-special.jpg',
+  },
+  {
+    id: 16,
+    name: 'War of the Worlds',
+    imgSrc: 'img/war-of-the-worlds.jpg',
+  },
+  {
+    id: 17,
+    name: 'Dardjeeling Limited',
+    imgSrc: 'img/dardjeeling-limited.jpg',
+  },
+  {
+    id: 18,
+    name: 'Orlando',
+    imgSrc: 'img/orlando.jpg',
+  },
+  {
+    id: 19,
+    name: 'Mindhunter',
+    imgSrc: 'img/mindhunter.jpg',
+  },
+  {
+    id: 20,
+    name: 'Midnight Special',
+    imgSrc: 'img/midnight-special.jpg',
+  },
+];
